ef4ba728d18a948e526961cba850f2b43b19082c
DESKTOP-B6HGP1L\Victoria Yen <good5538@hotmail.com>
2016-10-09 01:38:37 -0400

    import final wand

---
+++ script/other_movingUpAndDown.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class other_movingUpAndDown : MonoBehaviour {
+
+	public Transform spot_one;
+	public Transform spot_two;
+	public float frequence = 0.1f;
+	float rdFactor;
+	// Use this for initialization
+	void Start () {
+		rdFactor = Random.Range (0f,2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float whereNow = Mathf.Sin (Time.time * rdFactor *frequence)+1;//between 0~2
+		transform.position = spot_one.position+( (spot_two.position-spot_one.position)*(whereNow/2));
+	}
+}
---
+++ script/other_selfRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class other_selfRotation : MonoBehaviour {
+
+	public float anglePerFrame = 2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (transform.position,Vector3.up,Time.deltaTime*anglePerFrame);
+	}
+}
